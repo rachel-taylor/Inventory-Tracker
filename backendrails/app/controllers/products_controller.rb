cb4103e2df6f88e2054ec556f9cb1101b9216127
rachel-taylor <taylor.d.rachel@gmail.com>
2021-03-14 14:52:42 -0700

    Seeding Data

--- backendrails/app/controllers/products_controller.rb
+++ backendrails/app/controllers/products_controller.rb
@@ -3,4 +3,9 @@ class ProductsController < ApplicationController
         @products = Product.all
         render json: @products 
     end 
+
+    def show 
+        @product = Product.find_by(params[:id])
+        render json: @product
+    end 
 end
--- backendrails/db/seeds.rb
+++ backendrails/db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+15.times do 
+    Product.create(name: Faker::Coffee.blend_name, itemdetails: Faker::TvShows::Simpsons.quote, price: Faker::Commerce.price)
+end 
