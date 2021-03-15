9cd3b6491dec5d9ec517406f80e4a31782366b38
rachel-taylor <taylor.d.rachel@gmail.com>
2021-03-14 20:56:26 -0700

    Serializer

--- backendrails/app/models/product.rb
+++ backendrails/app/models/product.rb
@@ -1,3 +1,7 @@
 class Product < ApplicationRecord
     belongs_to :department
+
+    def department_name 
+        self.department.name 
+    end 
 end
---
+++ backendrails/app/serializers/product_serializer.rb
@@ -0,0 +1,5 @@
+class ProductSerializer < ActiveModel::Serializer
+  attributes :name, :price, :itemdetails, :department_name
+
+ 
+end
