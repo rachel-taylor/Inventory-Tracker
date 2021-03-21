366ab6fbfd58e9bc0d54b31c53bcbb484e7a46be
rachel-taylor <taylor.d.rachel@gmail.com>
2021-03-21 15:39:14 -0700

    Fixing Database

--- frontendjs/src/index.js
+++ frontendjs/src/index.js
@@ -1,19 +1,43 @@
 const BASE_URL = "http://localhost:3000"
 const homeEl = document.getElementById('home')
 
+function refreshProducts(){
+    homeEl.innerHTML = "<h3>Refreshing</h3>";
 fetch("http://localhost:3000/products")
     .then((res) => res.json()) 
     .then((data) => renderProducts(data));
+};
+refreshProducts()
 
 function formatPrice(price){
     return `$${price}`;
 };
 function formatDescription(itemdetails){
     return `${itemdetails}`
-}
+};
 function formatDepartment(department_name){
     return `${department_name}`
-}
+};
+
+function submitItem(data) {
+    fetch(`http://localhost:3000/products`, {
+      method: "POST",
+    })
+      .then((res) => res.json())
+      .then((product) => {
+        const newProduct = new Product(product);
+        Products.innerHTML += newProduct.renderProducts();
+      });
+  };
+
+function bindItemFormEventListener(){
+    productForm.addEventListener("submit", function (p) {
+        p.preventDefault();
+        var formData = new FormData(p.target);
+        submitItem(formData);
+        console.log(formData);
+    }
+    )}; 
  
 const renderProducts = function (products) {
     console.log(products);
